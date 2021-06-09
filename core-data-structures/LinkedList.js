b5668a6edba9778492d5a1699118d363f0717d3b
Jorge Lopez <je.lopez@me.com>
2021-06-08 23:16:48 -0700

    move a dir out

---
+++ core-data-structures/LinkedList.js
@@ -0,0 +1,157 @@
+import Node from './Node';
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(data) {
+    this.addTail(data);
+  }
+
+  addHead(data) {
+    if (!data) {
+      return undefined;
+    }
+
+    const headNode = new Node(data);
+
+    if (!this.head) {
+      this.head = headNode;
+    } else {
+      headNode.next = this.head;
+      this.head = headNode;
+    }
+
+    this.size += 1;
+  }
+
+  addTail(data) {
+    if (!data) {
+      return undefined;
+    }
+
+    const tailNode = new Node(data);
+    let node = this.head;
+
+    if (!node) {
+      this.head = tailNode;
+    } else {
+      while (node.next) {
+        node = node.next;
+      }
+
+      node.next = tailNode;
+    }
+
+    this.size += 1;
+  }
+
+  remove(data) {
+    let prevNode = null;
+    let currNode = this.head;
+
+    while (currNode && data) {
+      if (currNode.data === data) {
+        this.size -= 1;
+
+        if (!prevNode) {
+          this.head = null;
+        } else {
+          prevNode.next = currNode.next;
+          return currNode;
+        }
+      }
+
+      prevNode = currNode;
+      currNode = prevNode.next;
+    }
+
+    return undefined;
+  }
+
+  removeHead() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    this.head = this.head.next;
+    this.size -= 1;
+  }
+
+  removeTail() {
+    let prevNode = null;
+    let currNode = this.head;
+
+    while (currNode) {
+      if (!currNode.next) {
+        this.size -= 1;
+
+        if (!prevNode) {
+          this.head = null;
+        } else {
+          prevNode.next = null;
+        }
+
+        return currNode;
+      }
+
+      prevNode = currNode;
+      currNode = prevNode.next;
+    }
+
+    return undefined;
+  }
+
+  contains(element) {
+    let node = this.head;
+
+    while (node) {
+      if (node.data === element) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  getHead() {
+    return this.head || 'empty';
+  }
+
+  getTail() {
+    let node = this.head;
+
+    while (node.next) {
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  peek() {
+    return this.head;
+  }
+
+  printList() {
+    let node = this.head;
+
+    console.log('List:');
+
+    if (!node) {
+      console.log('empty\n');
+    } else {
+      while (node) {
+        console.log(node.data);
+        node = node.next;
+      }
+
+      console.log('\n');
+    }
+  }
+}
---
+++ core-data-structures/Stack.js
@@ -0,0 +1,74 @@
+import Node from './Node';
+
+export default class Stack {
+  constructor(maxSize) {
+    this.top = null;
+    this.size = 0;
+    this.maxSize = maxSize || null;
+  }
+
+  push(data) {
+    let exit = false;
+
+    // TODO: test the log: combine these two checks and use && for the log, perhaps like "data && console.log(...)" ?
+    if (!data) {
+      exit = true;
+    }
+
+    if (this.size === this.maxSize) {
+      console.log(`Warning: Stack has already reached its capacity of ${this.maxSize}. Item "${data}" can not be added`);
+      exit = true;
+    }
+
+    if (exit) {
+      return undefined;
+    }
+
+    const node = new Node(data);
+
+    node.next = this.top;
+
+    this.size += 1;
+    this.top = node;
+  }
+
+  pop() {
+    if (!this.top) {
+      return undefined;
+    }
+
+    const node = this.top
+
+    this.top = node.next;
+    this.size -= 1;
+
+    return node;
+  }
+
+  peek() {
+    return this.top || 'empty';
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  getMaxSize() {
+    return this.maxSize || 'no limit set';
+  }
+
+  printStack() {
+    let node = this.top;
+
+    console.log('Stack:');
+
+    if(!node) {
+      console.log('empty\n');
+    } else {
+      while (node) {
+        console.log(node.data);
+        node = node.next;
+      }
+    }
+  }
+}
